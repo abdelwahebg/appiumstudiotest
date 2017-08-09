eb959c2b8c58686b73f3d19a7caac16b9fb79f9c
Ashish <ashish.p.deshmukh@gmail.com>
2017-08-09 13:55:06 +0530

    Started with test

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,3 +1,4 @@
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.IOSMobileCapabilityType;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.MobileBrowserType;
@@ -19,7 +20,7 @@ public class SampleTest {
     private String reportDirectory = "reports";
     private String reportFormat = "xml";
     private String testName = "Untitled";
-    protected IOSDriver driver = null;
+    protected IOSDriver driver;
 
     @BeforeTest
     public void setUp() throws MalformedURLException {
@@ -29,15 +30,14 @@ public class SampleTest {
         dc.setCapability("testName", testName);
         dc.setCapability(MobileCapabilityType.UDID, "5190f41ff54c980d4ecbfe3025f389bbeb951e1d");
         dc.setBrowserName(MobileBrowserType.SAFARI);
+        //dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.experitest.ExperiBank");
         driver = new IOSDriver(new URL("http://localhost:4723/wd/hub"), dc);
     }
 
     @Test
     public void testUntitled() {
-        driver.get("http://www.google.com");
-        driver.context("NATIVE_APP");
-        driver.hideKeyboard();
-        driver.sendKeys("{ENTER}");
+        driver.get("https://viu.com");
+
     }
 
     @AfterTest
